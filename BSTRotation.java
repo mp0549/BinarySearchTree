f322791444d811daa273c8996bb10506636f620e
mp0549 <127653829+mp0549@users.noreply.github.com>
2025-02-05 23:55:02 -0600

    Add files via upload

--- BSTRotation.java
+++ BSTRotation.java
@@ -25,11 +25,13 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree_Place
 
             // right rotation if child is a left child
             if (parent.childLeft() == child) {
+                //System.out.println("Right Rotation");
                 rightRotate(child , parent);
             }
 
             // left rotation if child is a right child
             else if (parent.childRight() == child) {
+                //System.out.println("Left Rotation");
                 leftRotate(child,parent);
             }
 
@@ -45,8 +47,12 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree_Place
         
     }
 
+    /**
+     * private helper to rotate the tree to the right
+     */
     private void rightRotate(BinaryTreeNode<T> child, BinaryTreeNode<T> parent) {
         
+        // making child the new parent
         child.setParent(parent.parent());
         if (parent.parent() != null) {
             if (parent.parent().childLeft() == parent) {
@@ -55,8 +61,12 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree_Place
                 parent.parent().setChildRight(child);
             }
         }
+        else {
+            this.root = child;
+        }
         parent.setParent(child);
 
+        // moving the children over
         if (child.childRight() != null) {
             parent.setChildLeft(child.childRight());
             child.childRight().setParent(parent);
@@ -71,7 +81,8 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree_Place
 
 
     private void leftRotate(BinaryTreeNode<T> child, BinaryTreeNode<T> parent) {
-
+        
+        // making child the new parent
         child.setParent(parent.parent());
         if (parent.parent() != null) {
             if (parent.parent().childLeft() == parent) {
@@ -80,67 +91,223 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree_Place
                 parent.parent().setChildRight(child);
             }
         }
-        
+        else {
+            this.root = child;
+        }
+
         parent.setParent(child);
 
-        if (parent.parent() != null) {
-            if (parent.parent().childLeft() == parent) {
-                parent.parent().setChildLeft(child);
-            } else {
-                parent.parent().setChildRight(child);
-            }
+        // moving the children over
+        if (child.childLeft() != null) {
+            parent.setChildRight(child.childLeft());
+            child.childLeft().setParent(parent);
+        } else {
+            parent.setChildRight(null);
         }
-        
 
         child.setChildLeft(parent);
         
     }
 
 
-    public boolean test1() {
-        BSTRotation<Integer> bstRotation = new BSTRotation<>();
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        BinarySearchTree_Placeholder<Integer> bst = new BinarySearchTree_Placeholder<>();
+
+    /**
+     * the first tester method checks if the rotate() method throws the correct exceptions
+     * @return
+     */
+    public boolean test1() {
+        // bstRotation object to do the actual testing
+        BSTRotation<Integer> bst = new BSTRotation<>();
+        //constructing tree
         bst.root = new BinaryTreeNode<Integer>(10);
         bst.root.setChildLeft(new BinaryTreeNode<Integer>(5));
         bst.root.setChildRight(new BinaryTreeNode<Integer>(14));
         bst.root.childLeft().setChildLeft(new BinaryTreeNode<Integer>(2));
         
+        // NullPointerException
         try {
-            bstRotation.rotate(bst.root.childLeft() , null);
+            bst.rotate(bst.root.childLeft() , null);
             return false;
         } catch (NullPointerException e) {
         
         }
 
         try {
-            bstRotation.rotate(null , bst.root.childRight());
+            bst.rotate(null , bst.root.childRight());
             return false;
         } catch (NullPointerException e) {
+
+        }
+
+        // IllegalArgumentException
+        try {
+            bst.rotate(bst.root.childLeft() , bst.root.childRight());
+            return false;
+        } catch (IllegalArgumentException e) {
+
+        }        
+        try {
+            bst.rotate(bst.root , bst.root);
+            return false;
+        } catch (IllegalArgumentException e) {
 
         }
 
         try {
-            bstRotation.rotate(bst.root.childLeft() , bst.root.childRight());
+            bst.rotate(bst.root , bst.root.childLeft());
             return false;
-        } catch (NullPointerException e) {
+        } catch (IllegalArgumentException e) {
 
-        }
-        
+        }                
         
         return true;
     }
 
+
+    /**
+     * Tests rotation() method for rotations not involving the rut node
+     * @return True if rotate() works correctly
+     */
     public boolean test2() {
+        // new BSTRotation object for testing
+        BSTRotation<Integer> bst = new BSTRotation<>();
+
+        // constructing the tree
+        bst.root = new BinaryTreeNode<Integer>(10);
+        bst.root.setChildLeft(new BinaryTreeNode<Integer>(5));
+        bst.root.childLeft().setParent(bst.root);
+        bst.root.setChildRight(new BinaryTreeNode<Integer>(14));
+        bst.root.childRight().setParent(bst.root);;
+        bst.root.childLeft().setChildLeft(new BinaryTreeNode<Integer>(2));
+        bst.root.childLeft().childLeft().setParent(bst.root.childLeft());
+        bst.root.childLeft().setChildRight(new BinaryTreeNode<Integer>(7));
+        bst.root.childLeft().childRight().setParent(bst.root.childLeft());
+
+        /*
+         * RIGHT ROTATION
+         * 
+         * before:
+         *              10
+         *      5               14
+         *  2       7       
+         * 
+         * expected after:
+         *                  10
+         *      2                   14
+         *          5
+         *              7
+         *  
+         */
+
+        bst.rotate(bst.root.childLeft().childLeft() , bst.root.childLeft());
+
+        // checking if all the nodes went to the right spots and if the parent and child assignments got updated
+        if (bst.root.getData() != 10) return false;
+        if (bst.root.childLeft().getData() != 2 || bst.root.childLeft().parent().getData() != 10) return false;
+        if (bst.root.childLeft().childLeft() != null || bst.root.childLeft().childRight().getData() != 5) return false;
+        if (bst.root.childLeft().childRight().getData() != 5 || bst.root.childLeft().childRight().parent().getData() != 2) return false;
+        if (bst.root.childLeft().childRight().childLeft() != null || bst.root.childLeft().childRight().childRight().getData() != 7) return false;
+        if (bst.root.childLeft().childRight().childRight().childLeft()  != null || bst.root.childLeft().childRight().childRight().childRight() != null) return false;
         
+        /*
+         * LEFT ROTATION
+         * 
+         *  before:
+         *                  10
+         *      2                   14
+         *          5
+         *              7
+         *  expected after:
+         *              10
+         *      5               14
+         *  2       7        
+         */
+
+        bst.rotate(bst.root.childLeft().childRight() , bst.root.childLeft());
         
+        // checking if all the nodes went to the right spots and if the parent and child assignments got updated
+        if (bst.root.getData() != 10) return false;        
+        if (bst.root.childLeft().getData() != 5 || bst.root.childLeft().parent().getData() != 10) return false;
+        if (bst.root.childLeft().childLeft().getData() != 2 || bst.root.childLeft().childRight().getData() != 7) return false;
+        if (bst.root.childLeft().childRight().getData() != 7 || bst.root.childLeft().childRight().parent().getData() != 5) return false;
+        if (bst.root.childLeft().childRight().childLeft() != null || bst.root.childLeft().childRight().childRight() != null) return false;
+        if (bst.root.childLeft().childLeft().childLeft() != null || bst.root.childLeft().childLeft().childRight() != null) return false;
+
         return true;
     }
 
+    /**
+     * Tests rotation() for rotations involving the rut node
+     * @return True if rotate() works correctly
+     */
     public boolean test3() {
+        // new BSTRotation object for testing
+        BSTRotation<Integer> bst = new BSTRotation<>();
+
+        // constructing the tree
+        bst.root = new BinaryTreeNode<Integer>(10);
+        bst.root.setChildLeft(new BinaryTreeNode<Integer>(5));
+        bst.root.childLeft().setParent(bst.root);
+        bst.root.setChildRight(new BinaryTreeNode<Integer>(14));
+        bst.root.childRight().setParent(bst.root);;
+        bst.root.childLeft().setChildLeft(new BinaryTreeNode<Integer>(2));
+        bst.root.childLeft().childLeft().setParent(bst.root.childLeft());
+        bst.root.childLeft().setChildRight(new BinaryTreeNode<Integer>(7));
+        bst.root.childLeft().childRight().setParent(bst.root.childLeft());
+
+        /*
+         * RIGHT ROTATION
+         * 
+         *  before:
+         *              10
+         *      5               14
+         *  2       7       
+         * 
+         * expected after:
+         *              5
+         *      2               10
+         *                  7       14    
+         *  
+         */
+
+        bst.rotate(bst.root.childLeft() , bst.root);
+
+        if (bst.root.getData() != 5) return false;
+        if (bst.root.childLeft().getData() != 2) return false;
+        if (bst.root.childRight().getData() != 10) return false;
+        if (bst.root.childRight().childLeft().getData() != 7) return false;
+        if (bst.root.childRight().childRight().getData() != 14) return false;
+
+
+        /**
+         * LEFT ROTATION
+         * 
+         * before:
+         *              5
+         *      2               10
+         *                  7       14
+         * 
+         * expected after:
+         *                  10
+         *          5               14
+         *      2       7
+         */
+
         
+        bst.rotate(bst.root.childRight() , bst.root);
         
+        // checking if all the nodes went to the right spots
+        if (bst.root.getData() != 10) return false;
+        if (bst.root.childRight().getData() != 14) return false;
+        if (bst.root.childLeft().getData() != 5) return false;
+        if (bst.root.childLeft().childLeft().getData() != 2) return false;
+        if (bst.root.childLeft().childRight().getData() != 7) return false;
+
+
         return true;
+        
     }
 
     public static void main(String[] args) {
